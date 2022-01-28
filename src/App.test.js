5bcfd0ec4f30e5c76bc4a3cc125ceedec358f144
Mubarak <imubarak2424@gmail.com>
2022-01-29 00:47:53 +0100

    tests - readme

--- src/App.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-
-test('renders learn react link', () => {
-  render();
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
