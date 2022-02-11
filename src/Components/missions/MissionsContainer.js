72642d3d0d131d031ad1ed0715c9bcdec4b4bb61
Mubarak <imubarak2424@gmail.com>
2022-02-11 21:41:05 +0100

    comment Missions container

--- src/Components/missions/MissionsContainer.js
+++ src/Components/missions/MissionsContainer.js
@@ -1,4 +1,7 @@
-/* eslint-disable no-console */
+/**
+ * Mission Container file handles querying and sorting of mission related data 
+ * from the redux store.
+ */
 
 import React from 'react';
 import { useSelector, shallowEqual, useDispatch } from 'react-redux';
@@ -9,6 +12,10 @@ const MissionsContainer = () => {
   const dispatch = useDispatch();
   const states = useSelector((state) => state.missionsReducer, shallowEqual);
 
+  /**
+   * @function bookMission - Handles dispatching resereve mission action to the reducer
+   * @param {string} id - The id of the selected mission to be booked 
+   */
   const bookMission = (id) => {
     dispatch(reserveMission(id));
   };
